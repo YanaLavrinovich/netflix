adb696c00b1d47cefb421db7175cf9a4de739e69
Yana_Laurynovich <Super_mario_02>
2021-02-22 01:01:06 +0300

    add storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,9 @@
+export const parameters = {
+    actions: {argTypesRegex: "^on[A-Z].*"},
+    backgrounds: {
+        default: 'grey',
+        values: [
+            {name: 'grey', value: '#424242'},
+        ],
+    },
+}
